2f97dbd86ed006355639369d6154490855cb28d7
FilipStam97 <49717071+FilipStam97@users.noreply.github.com>
2025-12-17 23:18:14 +0100

    finished the core of the project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # advanced-os-zephyr
+
+
+## Docker iamge for zephyr
+docker pull modularmx/zephyros:latest
+
+In your local machine choose a directory where your project and Zephyr sour code will live. Run the container with a shared folder using the --mount flag. And in my case the /home/user/workspace set the working folder inside my container.
+
+docker run -it --rm -w /home/user/workspace --mount type=bind,src="$(pwd)",dst=/home/user/workspace modularmx/zephyros:latest
+
+
+Once the container is running you can run the west tool to download Zephyr from its official repositories. This is going to take a while because it will download the entire project!!, something we should avoid using a manifest, but fort the “momento” I think is worthy.
+
+west init
+west update
+
+
+Build your project using west. -p always means make a new build directory (or rewrite the existing one), while app is the name of the directory with the source files. If you want to remove the compilation binaries for wherever reason just type west build -t pristine
+
+west build -p always app
---
+++ applications/app1/src/main.c
@@ -0,0 +1,68 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio/gpio_emul.h>
+#include <zephyr/drivers/uart.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <zephyr/sys/printk.h>
+
+#define THRESHOLD_C 25.0
+#define PERIOD_MS 1000
+
+
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+int get_led_output( const struct gpio_dt_spec *led );
+
+/* Simulated sensor producing pseudo-random values */
+static int simulated_temp_c(void)
+{
+    uint32_t t = k_uptime_get_32();
+    return 18 + (t % 13);   // values from 18 to 30
+}
+
+int main(void)
+{
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+
+    bool actuator_on = false;
+
+    printk("UART-simulated sensor demo (native_sim)\n");
+    printk("Temperature threshold = %d C\n\n", THRESHOLD_C);
+
+    if (!gpio_is_ready_dt(&led)) {
+        printk("LED GPIO not ready\n");
+        return 0;
+    }
+
+
+    while (1)
+    {
+  
+        int temp = simulated_temp_c();
+        printk("Sensor(temp) = %.2f C\n", temp);
+
+        if (!actuator_on && temp >= THRESHOLD_C) {
+            actuator_on = true;
+            gpio_pin_set_dt(&led, 1);
+            printk("---> ACTUATOR: LED ON\n");
+        } else if (actuator_on && temp < THRESHOLD_C) {
+            actuator_on = false;
+            gpio_pin_set_dt(&led, 0);
+            printk("---> ACTUATOR: LED OFF\n");
+        }
+        printk("Emulated LED pin value = %d\n\n", get_led_output(&led));
+         k_msleep(PERIOD_MS);
+    }
+
+    return 0;
+}
+
+int get_led_output( const struct gpio_dt_spec *led )
+{
+    /*ge the emulated device from the led0 node, wich is basically gpio0*/
+    const struct device *dev = device_get_binding(led->port->name);
+    return gpio_emul_output_get(dev, 0);
+}
+
